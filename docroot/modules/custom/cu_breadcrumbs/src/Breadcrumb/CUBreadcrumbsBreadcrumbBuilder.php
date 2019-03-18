cd3e5ea990242b1b9dc1e71a06ff9c38c5aa0312
MaxTMcCoy <maxmccoy@creighton.edu>
2019-03-18 13:45:22 -0500

    user defined application of custom breadcrumbs

--- docroot/modules/custom/cu_breadcrumbs/src/Breadcrumb/CUBreadcrumbsBreadcrumbBuilder.php
+++ docroot/modules/custom/cu_breadcrumbs/src/Breadcrumb/CUBreadcrumbsBreadcrumbBuilder.php
@@ -2,14 +2,13 @@
 
 namespace Drupal\cu_breadcrumbs\Breadcrumb;
 
+use Drupal\node\NodeInterface;
+use Drupal\node\Entity\Node;
 use Drupal\Core\Breadcrumb\Breadcrumb;
 use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Menu;
-use Drupal\Core\Menu\MenuLinkTreeInterface;
 use Drupal\Core\Url;
 use Drupal\Core\Link;
-use Drupal\node\NodeInterface;
 use Drupal\Component\Utility\UrlHelper;
 
 /**
@@ -17,92 +16,97 @@ use Drupal\Component\Utility\UrlHelper;
  */
 class CUBreadcrumbsBreadcrumbBuilder implements BreadcrumbBuilderInterface {
 
-    /**
-     * Checks content type of current node to determine if it gets a breadcrumb.
-     */
-    public function applies(RouteMatchInterface $attributes) {
-      $node = $attributes->getParameter('node');
-      //get node object, node revisions are not the same as nodes and are not passed as objects
-      if(gettype($node)==='string')
-        $node = \Drupal\node\Entity\Node::load($attributes->getParameter('node'));
-      //if there's a node, do the code
-      if (!empty($node)) {
-        /** 
-         * these are the types of nodes that breadcrums will be applied to.
-         * (I'm just following the old code here)
-         * needs to be user defined instead of hard coded most likely
-         */
-        $applicable_node_types = ['content_page'
-                                  ,'news_spotlight'
-                                  ,'profile'];
-        //look to see if the node type is in $applicable_node_types, if it is true, else false
-        return in_array($node->getType(),$applicable_node_types);
-      }
-      // not super certain if something needs to happen if the node is empty
-      // again following old code, I'll look into it.
+  /**
+   * Checks content type of current node to determine if it gets a breadcrumb.
+   */
+  public function applies(RouteMatchInterface $attributes) {
+
+    $node = $attributes->getParameter('node');
+    // Get node object, node revisions are not the same as nodes and are not passed as objects.
+    if (gettype($node) === 'string') {
+      $node = Node::load($attributes->getParameter('node'));
     }
+    // If there's a node, do the code.
+    if (!empty($node)) {
+      $node_type = $node->type->entity;
+      /**
+         * look and see if content type (node_type) has breadcrumbs applied
+         */
+        $database = \Drupal::database();
+        //query the cu_breadcrumbs table
+        $query = $database->select('cu_breadcrumbs', 'cb')
+        //using the node_type uuid
+          ->condition('cb.n_uuid',$node_type->get('uuid'), '=')
+        //return apply
+          ->fields('cb', ['apply']);
 
-    /**
-     * Builds the breadcrumb.
-     */
-    public function build(RouteMatchInterface $route_match) {
-      $breadcrumb = new Breadcrumb();
-      // Create first breadcrumb to www.creighton.edu.
-      $url = Url::fromUri('https://www.creighton.edu');
-      $breadcrumb->addLink(Link::fromTextAndUrl('Home', $url));
+        $record = $query->execute()->fetch();
+        //if not empty return value (1 or 0) true of false
+      return !empty($record)?$record->apply:0;
+    }
+  }
 
-      // Get and loop through breadcrumbs menu.
-      $menu_name = 'breadcrumbs-menu';
-      $menu_tree = \Drupal::menuTree();
-      $parameters = $menu_tree->getCurrentRouteMenuTreeParameters($menu_name);
-      $tree = $menu_tree->load($menu_name, $parameters);
-      if (count($tree) > 0) {
-        $manipulators = [
+  /**
+   * Builds the breadcrumb.
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    // Create first breadcrumb to www.creighton.edu.
+    $url = Url::fromUri('https://www.creighton.edu');
+    $breadcrumb->addLink(Link::fromTextAndUrl('Home', $url));
+
+    // Get and loop through breadcrumbs menu.
+    $menu_name = 'breadcrumbs-menu';
+    $menu_tree = \Drupal::menuTree();
+    $parameters = $menu_tree->getCurrentRouteMenuTreeParameters($menu_name);
+    $tree = $menu_tree->load($menu_name, $parameters);
+    if (count($tree) > 0) {
+      $manipulators = [
           // Only show links that are accessible for the current user.
           ['callable' => 'menu.default_tree_manipulators:checkAccess'],
           // Use the default sorting of menu links.
           ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
-        ];
-        $tree = $menu_tree->transform($tree, $manipulators);
-        $menu_tmp = $menu_tree->build($tree);
-        if ($menu_tmp['#items']) {
-          foreach ($menu_tmp['#items'] as $item) {
-            $item_url = $item['url']->toString();
-            if (UrlHelper::isExternal($item_url)) {
-              $breadcrumb->addLink(Link::fromTextAndUrl($item['title'], $item['url']));
+      ];
+      $tree = $menu_tree->transform($tree, $manipulators);
+      $menu_tmp = $menu_tree->build($tree);
+      if ($menu_tmp['#items']) {
+        foreach ($menu_tmp['#items'] as $item) {
+          $item_url = $item['url']->toString();
+          if (UrlHelper::isExternal($item_url)) {
+            $breadcrumb->addLink(Link::fromTextAndUrl($item['title'], $item['url']));
+          }
+          else {
+            if ($item['url']->getRouteName() == 'front') {
+              $breadcrumb->addLink(Link::fromTextAndUrl($item['title'], $item['url']->getRouteName()));
             }
             else {
-              if ($item['url']->getRouteName() == 'front') {
-                $breadcrumb->addLink(Link::fromTextAndUrl($item['title'], $item['url']->getRouteName()));
-              }
-              else {
-                $breadcrumb->addLink(Link::fromTextAndUrl($item['title'], $item['url']->getInternalPath()));
-              }
+              $breadcrumb->addLink(Link::fromTextAndUrl($item['title'], $item['url']->getInternalPath()));
             }
           }
         }
       }
-      // Create breadcrumbs from current node and parents in main nav menu.
-      $node = \Drupal::routeMatch()->getParameter('node');
+    }
+    // Create breadcrumbs from current node and parents in main nav menu.
+    $node = \Drupal::routeMatch()->getParameter('node');
 
-      if ($node instanceof \Drupal\node\NodeInterface) {
-        $nid = $node->id();
-      }
-      $menu_link_manager = \Drupal::service('plugin.manager.menu.link');
-      $links = $menu_link_manager->loadLinksByRoute('entity.node.canonical', array('node' => $nid));
-      if ($link = reset($links)) {
-        if ($link->getParent()) {
-          $parents = array_reverse($menu_link_manager->getParentIds($link->getParent()));
-          foreach ($parents as $parent) {
-            $parent = $menu_link_manager->createInstance($parent);
-            $parent_title = $parent->getTitle();
-            $parent_url = $parent->getUrlObject();
-            $breadcrumb->addLink(Link::fromTextAndUrl($parent_title, $parent_url));
-          }
+    if ($node instanceof NodeInterface) {
+      $nid = $node->id();
+    }
+    $menu_link_manager = \Drupal::service('plugin.manager.menu.link');
+    $links = $menu_link_manager->loadLinksByRoute('entity.node.canonical', ['node' => $nid]);
+    if ($link = reset($links)) {
+      if ($link->getParent()) {
+        $parents = array_reverse($menu_link_manager->getParentIds($link->getParent()));
+        foreach ($parents as $parent) {
+          $parent = $menu_link_manager->createInstance($parent);
+          $parent_title = $parent->getTitle();
+          $parent_url = $parent->getUrlObject();
+          $breadcrumb->addLink(Link::fromTextAndUrl($parent_title, $parent_url));
         }
       }
-      return $breadcrumb;
-    // }
+    }
+    return $breadcrumb;
+    // }.
   }
 
 }
