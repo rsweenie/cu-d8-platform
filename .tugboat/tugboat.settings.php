d67227a3c86aaf4812959a080ac11319ea3d9207
Robert Wohleb <rwohleb@r2integrated.com>
2019-06-09 23:40:58 -0700

    Initial grad site tugboat settings

--- .tugboat/tugboat.settings.php
+++ .tugboat/tugboat.settings.php
@@ -10,3 +10,40 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 );
+
+/**
+ * Public file path:
+ *
+ * A local file system path where public files will be stored. This directory
+ * must exist and be writable by Drupal. This directory must be relative to
+ * the Drupal installation directory and be accessible over the web.
+ */
+$settings['file_public_path'] = 'sites/default/files';
+
+/**
+ * Private file path:
+ *
+ * A local file system path where private files will be stored. This directory
+ * must be absolute, outside of the Drupal installation directory and not
+ * accessible over the web.
+ *
+ * Note: Caches need to be cleared when this value is changed to make the
+ * private:// stream wrapper available to the system.
+ *
+ * See https://www.drupal.org/documentation/modules/file for more information
+ * about securing private files.
+ */
+# $settings['file_private_path'] = '';
+
+/**
+ * Temporary file path:
+ *
+ * A local file system path where temporary files will be stored. This
+ * directory should not be accessible over the web.
+ *
+ * Note: Caches need to be cleared when this value is changed.
+ *
+ * See https://www.drupal.org/node/1928898 for more information
+ * about global configuration override.
+ */
+$config['system.file']['path']['temporary'] = '/tmp';
