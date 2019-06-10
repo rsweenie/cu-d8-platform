4ffec772b6e62c8167f6a4359b6744f4f152671a
Robert Wohleb <rwohleb@r2integrated.com>
2019-06-10 01:00:19 -0700

    Fixed file paths

--- .tugboat/init-db.sh
+++ .tugboat/init-db.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DB_FILE="$TUGBOAT_ROOT/$TUGBOAT_GITHUB_HEAD.sql.gz"
+DB_FILE="$TUGBOAT_ROOT/.tugboat/db/$TUGBOAT_GITHUB_HEAD.sql.gz"
 
 #
 # Tries to import a branch specific DB first, else falls back on a base copy.
 #
-if [ -f "$DB_FILE"]; then
+if [[ -f "$DB_FILE" ]]; then
   zcat "$DB_FILE" | mysql tugboat
 else
-  zcat "$TUGBOAT_ROOT/base.sql.gz" | mysql tugboat
+  zcat "$TUGBOAT_ROOT/.tugboat/db/base.sql.gz" | mysql tugboat
 fi
