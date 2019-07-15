45858ead736878419399f857bfe31d54e77eaa45
Aaron Weidele <aweidele@r2integrated.com>
2019-07-15 17:11:36 -0400

    CU-96: Image/video styling

--- docroot/themes/custom/cu2019_grad/assets/js/site.js
+++ docroot/themes/custom/cu2019_grad/assets/js/site.js
@@ -8,6 +8,14 @@
         $(this).css({'background-image':'url('+bg+')'});
         $(this).addClass('js-background');
       });
+
+      $('figure.img_vid.video', context).each(function() {
+        $(this).on('click', function() {
+          var src = $('iframe', this).attr('src') + '&autoplay=1';
+          $('iframe', this).attr('src', src);
+          $(this).addClass('playing');
+        })
+      });
     }
 };
 })(jQuery, Drupal);
