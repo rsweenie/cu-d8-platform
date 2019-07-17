b35dce5b94aa44f7c8010d0e52417f82f2f4d498
Aaron Weidele <aweidele@r2integrated.com>
2019-07-17 13:50:54 -0400

    CU-96: Video styling and functionality

--- docroot/themes/custom/cu2019_grad/assets/js/site.js
+++ docroot/themes/custom/cu2019_grad/assets/js/site.js
@@ -9,13 +9,13 @@
         $(this).addClass('js-background');
       });
 
-      $('figure.img_vid.video', context).each(function() {
-        $(this).on('click', function() {
-          var src = $('iframe', this).attr('src') + '&autoplay=1';
-          $('iframe', this).attr('src', src);
-          $(this).addClass('playing');
-        })
-      });
+      // $('figure.img_vid.video', context).each(function() {
+      //   $(this).on('click', function() {
+      //     var src = $('iframe', this).attr('src') + '&autoplay=1';
+      //     $('iframe', this).attr('src', src);
+      //     $(this).addClass('playing');
+      //   })
+      // });
     }
 };
 })(jQuery, Drupal);
