121f08bd175f212a5db28c02d56d4f577f9c9062
Aaron Weidele <aweidele@r2integrated.com>
2019-07-17 11:55:31 -0400

    CU-143 Open external links in new tab

--- docroot/themes/custom/cu2019_grad/assets/js/site.js
+++ docroot/themes/custom/cu2019_grad/assets/js/site.js
@@ -3,10 +3,23 @@
 (function ($, Drupal) {
   Drupal.behaviors.fsbg = {
     attach: function (context, settings) {
+      // Hide image and add background image
       $('.card.img-behind',context).each(function() {
         var bg = $('img',this).attr('src');
         $(this).css({'background-image':'url('+bg+')'});
         $(this).addClass('js-background');
+      });
+
+      // Open all external links in new tab
+      $("a", context).each(function() {
+        var a = new RegExp('/' + window.location.host + '/');
+         if(!a.test(this.href)) {
+             $(this).click(function(event) {
+                 event.preventDefault();
+                 event.stopPropagation();
+                 window.open(this.href, '_blank');
+             });
+         }
       });
     }
 };
