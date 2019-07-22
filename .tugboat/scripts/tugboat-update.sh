06406f8e1349573346109a99c18c226fcb707eb2
Harry Slaughter <harry@slaughters.com>
2019-07-22 14:55:07 -0500

    Ticket #375584714 - debugging no mysql client issue again

--- .tugboat/scripts/tugboat-update.sh
+++ .tugboat/scripts/tugboat-update.sh
@@ -27,8 +27,11 @@ case $CU_SITE_ALIAS in
 
   # DEFAULT
   # TODO: Possibly do a base db install. Myabe using drush install? Something we can test the core codebase with,
-  # something not tied to a specific site.
+  # something not tied to a specific site. Meanwhile, we'll use Alliance as our base.
   *)
+
+    drush -r "${DOCROOT}" sql:sync "@alliance.01live" @self -y
+    drush -r "${DOCROOT}" rsync "@alliance.01live":%files @self:%files -y
 
   ;;
 esac
