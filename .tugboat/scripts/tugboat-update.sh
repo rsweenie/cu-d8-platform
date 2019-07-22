501e5203c0d6040d3e9e273db8910da9f07314b1
Harry Slaughter <harry@slaughters.com>
2019-07-22 15:45:25 -0500

    Ticket #375584714 - WIP debugging 'empty' site install

--- .tugboat/scripts/tugboat-update.sh
+++ .tugboat/scripts/tugboat-update.sh
@@ -9,8 +9,11 @@ CU_SITE_ALIAS=$(`dirname "$0"`/get_site_alias.sh)
 case $CU_SITE_ALIAS in
   alliance)
   # DB sync MUST come before filesync always
-    drush -r "${DOCROOT}" sql:sync "@alliance.01live" @self -y
-    drush -r "${DOCROOT}" rsync "@alliance.01live":%files @self:%files -y
+#    drush -r "${DOCROOT}" sql:sync "@alliance.01live" @self -y
+#    drush -r "${DOCROOT}" rsync "@alliance.01live":%files @self:%files -y
+
+    # Debugging
+    drush -r "${DOCROOT}" site-install creighton -y
   ;;
   grad-site)
     DB_FILE="${TUGBOAT_ROOT}/.tugboat/db/${TUGBOAT_GITHUB_HEAD}.sql.gz"
