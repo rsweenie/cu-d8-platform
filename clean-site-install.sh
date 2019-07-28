bc1b44f69a28b886178500b0197ae841a487d2d9
Abraham <abrahamcoleman@creighton.edu>
2019-07-27 19:58:36 -0700

    i did a thing with some stuff

---
+++ clean-site-install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Rub bare site install
+
+echo 'Please enter the name of the site you would like to copy files from (alliance, hrnew): '
+read site_name
+
+echo 'Please select the local enviroment you are using (vagrant, lando): '
+devoptions=('vagrant' 'lando')
+declare local
+select option in "${devoptions[@]}"
+do
+    case $option in 
+        'vagrant'|'lando')
+            local=$option
+            break
+            ;;
+        *)
+            echo "$REPLY is not a valid option. Please try again: "
+            ;;
+    esac
+done
+
+echo 'Please select the site environment you want to use (dev, live, test): '
+options=('01live' '01test' '01dev')
+declare env
+select option in "${options[@]}"
+do
+    case $option in 
+        '01live'|'01test'|'01dev')
+            env=$option
+            break
+            ;;
+        *)
+            echo "$REPLY is not a valid option. Please try again: "
+            ;;
+    esac
+done
+
+if [[ $local = "lando" ]]
+then
+    local=$local
+elif [[ $local = "vagrant" ]]
+then
+    local=""
+fi
+
+site_alias="@$site_name.$env"
+
+# Does all the things from the selected Acquia site to the local environment
+eval $local drush sql-drop -y; $local drush sql:sync $site_alias @self -y; $local drush rsync $site_alias:%files @self:%files -y; $local composer install; $local drush cim -y; $local drush updatedb -y; $local drush cron; $local drush cr; $local drush uli 
+
