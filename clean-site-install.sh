407f81358039b6914171810b3f1c60c39add3537
Abraham Coleman <abraham@abaracus.com>
2019-07-29 10:38:09 -0500

    new site or nah

--- clean-site-install.sh
+++ clean-site-install.sh
@@ -25,27 +25,36 @@ then
     local=""
 fi
 
-echo 'Please enter the name of the site you would like to copy files from (alliance, hrnew, demo, etc.): '
-read site_name
+echo 'Do you want to sync from a database/site? [Y,n]: '
+read new_database
+if [[ $new_database == "Y" || $new_database == "y" ]]; then            
 
-echo 'Please select the site environment you want to use (live, test, dev): '
-options=('01live' '01test' '01dev')
-declare env
-select option in "${options[@]}"
-do
-    case $option in 
-        '01live'|'01test'|'01dev')
-            env=$option
-            break
-            ;;
-        *)
-            echo "$REPLY is not a valid option. Please try again: "
-            ;;
-    esac
-done
+    echo 'Please enter the name of the site you would like to copy files from (alliance, hrnew, demo, etc.): '
+    read site_name
 
-site_alias="@$site_name.$env"
+    echo 'Please select the site environment you want to use (live, test, dev): '
+    options=('01live' '01test' '01dev')
+    declare env
+    select option in "${options[@]}"
+    do
+        case $option in 
+            '01live'|'01test'|'01dev')
+                env=$option
+                break
+                ;;
+            *)
+                echo "$REPLY is not a valid option. Please try again: "
+                ;;
+        esac
+    done
 
-# Does all the things from the selected Acquia site to the local environment
-eval $local drush sql-drop -y; $local drush sql:sync $site_alias @self -y; $local drush rsync $site_alias:%files @self:%files -y; $local composer install; $local drush updatedb -y; $local drush cim -y; $local drush cr; $local drush cron; $local drush uli 
+    site_alias="@$site_name.$env"
+
+    # Does all the things from the selected Acquia site to the local environment
+    eval $local drush sql-drop -y; $local drush sql:sync $site_alias @self -y; $local drush rsync $site_alias:%files @self:%files -y; $local composer install; $local drush updatedb -y; $local drush cim -y; $local drush cr; $local drush cron; $local drush uli 
+
+else
+    eval $local composer install; $local drush updatedb -y; $local drush cim -y; $local drush cr; $local drush cron; $local drush uli
+fi
+
 
