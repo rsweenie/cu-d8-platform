49e4ca80e376b5bfaf48dc2d3bb8ff9c6dadfaec
MaxTMcCoy <maxmccoy@creighton.edu>
2019-08-08 14:37:19 -0500

    remove acsf modules from default core extensions acquia is using config split now for different envs, update to acsf 2.59

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -1,7 +1,10 @@
 <?php
 
 // ===== Added by acsf-init, please do not delete. Section start. =====
-include dirname(__FILE__) . '/acsf.settings.php';
+$_acsf_infrastructure = include dirname(__FILE__) . '/acsf.settings.php';
+if ($_acsf_infrastructure === 'acsf-infrastructure') {
+  return;
+}
 // ===== Added by acsf-init, please do not delete. Section end. =====
 
 
