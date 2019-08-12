c6e55858d3654f8cb789593c7dd2f5c2592410a5
MaxTMcCoy <maxmccoy@creighton.edu>
2019-08-12 15:23:32 -0500

    test relative path for sso-config

--- factory-hooks/post-install/cu-sso-config.php
+++ factory-hooks/post-install/cu-sso-config.php
@@ -8,16 +8,16 @@
  *
  */
 
-$site = getenv('AH_SITE_GROUP');
-$env = getenv('AH_SITE_ENVIRONMENT');
-echo "ENVIRONMENT: $env";
+$site = $_ENV['AH_SITE_GROUP'];
+$env = $_ENV['AH_SITE_ENVIRONMENT'];
+$target_env = $site . $env;
 // The public domain name of the website.
 // Run updates against requested domain rather than acsf primary domain.
-$domain = getenv('HTTP_HOST');
+$domain = $_SERVER['HTTP_HOST'];
 $domain_fragments = explode('.', $domain);
 $site_name = array_shift($domain_fragments);
 
 exec("/mnt/www/html/$site.$env/vendor/acquia/blt/bin/blt drupal:update --environment=$env --site=$site_name --define drush.uri=$domain --verbose --yes");
 echo "ENVIRONMENT: $env";
 // Run sso-config.sh to set sp_entity_id in config when a new site is created.
-exec("bash ../../sso-config.sh $site_name $env");
+exec("bash ../sso-config.sh $site_name $env");
