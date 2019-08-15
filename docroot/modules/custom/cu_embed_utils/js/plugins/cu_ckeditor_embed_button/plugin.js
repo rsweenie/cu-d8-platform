10a56849da1376e288e7bbad5775710c9979a991
Rick Sweenie <rsweenie@gmail.com>
2019-08-15 13:21:25 -0500

    Commenting out plugin config option for further testing.

--- docroot/modules/custom/cu_embed_utils/js/plugins/cu_ckeditor_embed_button/plugin.js
+++ docroot/modules/custom/cu_embed_utils/js/plugins/cu_ckeditor_embed_button/plugin.js
@@ -3,39 +3,39 @@
  * Drupal Entity embed plugin.
  */
 
-//(function ($, Drupal, CKEDITOR) {
+(function ($, Drupal, CKEDITOR) {
 
   "use strict";
 
-  //CKEDITOR.plugins.add('drupalentity', {
+  CKEDITOR.plugins.add('drupalentity', {
     // This plugin requires the Widgets System defined in the 'widget' plugin.
-    //requires: 'widget',
-//
-    //// The plugin initialization logic goes inside this method.
-    //beforeInit: function (editor) {
-    //  // Configure CKEditor DTD for custom drupal-entity element.
-    //  // @see https://www.drupal.org/node/2448449#comment-9717735
-    //  var dtd = CKEDITOR.dtd, tagName;
-    //  dtd['drupal-entity'] = {'#': 1};
-    //  // Register drupal-entity element as allowed child, in each tag that can
-    //  // contain a div element.
-    //  for (tagName in dtd) {
-    //    if (dtd[tagName].div) {
-    //      dtd[tagName]['drupal-entity'] = 1;
-    //    }
-    //  }
-    //},
-    //instanceReady: function(editor) {
-    //  var dtd = CKEDITOR.dtd;
-//
-    //  console.log(dtd);
-    //  // Display the drupal-entity element inline.
-    //  dtd.$inline['drupal-entity'] = 1;
-    //  dtd['p']['drupal-entity'] = 1;
-    //  dtd['span']['drupal-entity'] = 1;
-    //  dtd['span']['article'] = 1;
-//
-    //  alert("Who Farted?!");
-    //}
-//  });
-//})(jQuery, Drupal, CKEDITOR);
+    requires: 'widget',
+
+    // The plugin initialization logic goes inside this method.
+    beforeInit: function (editor) {
+      // Configure CKEditor DTD for custom drupal-entity element.
+      // @see https://www.drupal.org/node/2448449#comment-9717735
+      var dtd = CKEDITOR.dtd, tagName;
+      dtd['drupal-entity'] = {'#': 1};
+      // Register drupal-entity element as allowed child, in each tag that can
+      // contain a div element.
+      for (tagName in dtd) {
+        if (dtd[tagName].div) {
+          dtd[tagName]['drupal-entity'] = 1;
+        }
+      }
+    },
+    instanceReady: function(editor) {
+      var dtd = CKEDITOR.dtd;
+
+      console.log(dtd);
+      // Display the drupal-entity element inline.
+      dtd.$inline['drupal-entity'] = 1;
+      dtd['p']['drupal-entity'] = 1;
+      dtd['span']['drupal-entity'] = 1;
+      dtd['span']['article'] = 1;
+
+      alert("Who Farted?!");
+    }
+  });
+})(jQuery, Drupal, CKEDITOR);
