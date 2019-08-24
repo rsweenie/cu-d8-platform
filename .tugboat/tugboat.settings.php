81ebb5275c3f369b02f128f9de6f5bd1dd3c5c7c
Robert Wohleb <rwohleb@r2integrated.com>
2019-08-23 18:23:05 -0700

    Fix system call that switches settings file.

--- .tugboat/tugboat.settings.php
+++ .tugboat/tugboat.settings.php
@@ -14,7 +14,7 @@ $databases['default']['default'] = array (
 
 
 // Site specific Drupal settings
-system($_ENV['TUGBOAT_ROOT'] . '/.tugboat/scripts/get_site_alias.sh > /dev/null', $cu_site_name);
+$cu_site_name = system($_ENV['TUGBOAT_ROOT'] . '/.tugboat/scripts/get_site_alias.sh');
 
 switch ($cu_site_name) {
   case 'alliance':
