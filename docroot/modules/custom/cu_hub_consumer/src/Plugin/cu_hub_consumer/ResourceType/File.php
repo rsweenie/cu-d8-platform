d5cbaf7d3337b61656af07f6d9832800bf32403e
Robert Wohleb <rwohleb@r2integrated.com>
2019-09-06 10:56:35 -0700

    Added file resource plugin.

---
+++ docroot/modules/custom/cu_hub_consumer/src/Plugin/cu_hub_consumer/ResourceType/File.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\cu_hub_consumer\Plugin\cu_hub_consumer\ResourceType;
+
+use Drupal\cu_hub_consumer\Hub\ResourceTypeBase;
+
+/**
+ * Undocumented class
+ * 
+ * @HubResourceType(
+ *   id = "file",
+ *   label = @Translation("File resource type"),
+ *   description = @Translation("Resource type for file."),
+ *   hub_type_id = "file--file",
+ * )
+ */
+class File extends ResourceTypeBase {
+
+}
