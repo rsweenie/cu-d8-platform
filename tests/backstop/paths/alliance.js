bda6184cc55a2ade338ede4b702ec641baa5c455
Abe Coleman <abraham@abaracus.com>
2019-09-19 12:50:06 -0700

    paths folder and url folder

--- tests/backstop/paths/alliance.js
+++ tests/backstop/paths/alliance.js
@@ -1,19 +1,9 @@
 var pathConfig = {};
-var hrNewUrls = {};
-
 pathConfig.array = [
   '/about',
   '/residencies-fellowships',
-],
-
-hrNewUrls.array = [
-    '/node/8426',
 ]
-   
- 
 module.exports = pathConfig;
-
-
  
 
 
