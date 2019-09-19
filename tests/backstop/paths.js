2a58d0af3b799638a2b76be78d40f88b166954a5
Abe Coleman <abraham@abaracus.com>
2019-09-19 11:58:07 -0700

    its a start

---
+++ tests/backstop/paths.js
@@ -0,0 +1,20 @@
+var pathConfig = {};
+var hrNewUrls = {};
+
+pathConfig.array = [
+  '/about',
+  '/residencies-fellowships',
+],
+
+hrNewUrls.array = [
+    '/node/8426',
+]
+   
+ 
+module.exports = pathConfig;
+
+
+ 
+
+
+
