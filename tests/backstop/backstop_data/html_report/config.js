a682e75e8c8d113b728b6c2bf5a39c5ea0f2f00d
Abe Coleman <abraham@abaracus.com>
2019-09-20 13:22:00 -0500

    more onfig

--- tests/backstop/backstop_data/html_report/config.js
+++ tests/backstop/backstop_data/html_report/config.js
@@ -3,72 +3,78 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "../bitmaps_reference/prod_test__node_8426_0_document_0_phone.png",
-        "test": "../bitmaps_test/20190920-090225/prod_test__node_8426_0_document_0_phone.png",
+        "reference": "../bitmaps_reference/prod_test___0_document_0_phone.png",
+        "test": "../bitmaps_test/20190920-112812/prod_test___0_document_0_phone.png",
         "selector": "document",
-        "fileName": "prod_test__node_8426_0_document_0_phone.png",
-        "label": "/node/8426",
+        "fileName": "prod_test___0_document_0_phone.png",
+        "label": "/",
         "misMatchThreshold": 0.1,
-        "url": "http://local.creighton.com//node/8426",
-        "referenceUrl": "https://alliance.creighton.edu/node/8426",
+        "url": "http://local.creighton.com//",
+        "referenceUrl": "https://alliance.creighton.edu/",
         "expect": 0,
         "viewportLabel": "phone",
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": 0
+            "height": 25
           },
-          "misMatchPercentage": "0.00"
-        }
+          "misMatchPercentage": "44.07",
+          "analysisTime": 146
+        },
+        "diffImage": "../bitmaps_test/20190920-112812/failed_diff_prod_test___0_document_0_phone.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/prod_test__node_8426_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20190920-090225/prod_test__node_8426_0_document_1_tablet.png",
+        "reference": "../bitmaps_reference/prod_test___0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190920-112812/prod_test___0_document_1_tablet.png",
         "selector": "document",
-        "fileName": "prod_test__node_8426_0_document_1_tablet.png",
-        "label": "/node/8426",
+        "fileName": "prod_test___0_document_1_tablet.png",
+        "label": "/",
         "misMatchThreshold": 0.1,
-        "url": "http://local.creighton.com//node/8426",
-        "referenceUrl": "https://alliance.creighton.edu/node/8426",
+        "url": "http://local.creighton.com//",
+        "referenceUrl": "https://alliance.creighton.edu/",
         "expect": 0,
         "viewportLabel": "tablet",
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": 0
+            "height": -35
           },
-          "misMatchPercentage": "0.00"
-        }
+          "misMatchPercentage": "38.75",
+          "analysisTime": 155
+        },
+        "diffImage": "../bitmaps_test/20190920-112812/failed_diff_prod_test___0_document_1_tablet.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
-        "reference": "../bitmaps_reference/prod_test__node_8426_0_document_2_desktop_display.png",
-        "test": "../bitmaps_test/20190920-090225/prod_test__node_8426_0_document_2_desktop_display.png",
+        "reference": "../bitmaps_reference/prod_test___0_document_2_desktop_display.png",
+        "test": "../bitmaps_test/20190920-112812/prod_test___0_document_2_desktop_display.png",
         "selector": "document",
-        "fileName": "prod_test__node_8426_0_document_2_desktop_display.png",
-        "label": "/node/8426",
+        "fileName": "prod_test___0_document_2_desktop_display.png",
+        "label": "/",
         "misMatchThreshold": 0.1,
-        "url": "http://local.creighton.com//node/8426",
-        "referenceUrl": "https://alliance.creighton.edu/node/8426",
+        "url": "http://local.creighton.com//",
+        "referenceUrl": "https://alliance.creighton.edu/",
         "expect": 0,
         "viewportLabel": "desktop display",
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": 0
+            "height": 16
           },
-          "misMatchPercentage": "0.00"
-        }
+          "misMatchPercentage": "42.39",
+          "analysisTime": 226
+        },
+        "diffImage": "../bitmaps_test/20190920-112812/failed_diff_prod_test___0_document_2_desktop_display.png"
       },
-      "status": "pass"
+      "status": "fail"
     }
   ],
   "id": "prod_test"
