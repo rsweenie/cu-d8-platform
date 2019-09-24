3c73891d631786e650567e16600842f818c43880
Abe Coleman <abraham@abaracus.com>
2019-09-24 08:23:24 -0500

    paths for hrnew and demo

--- tests/backstop/paths/hrnew.js
+++ tests/backstop/paths/hrnew.js
@@ -1,5 +1,14 @@
 var pathConfig = {};
 pathConfig.array = [
     '/',
+    '/about',
+    '/employee-resources',
+    '/employee-resources/benefits',
+    '/myhr',
+    '/myhr/performance-management',
+    '/new-hire-information',
+    
+
+
 ]
 module.exports = pathConfig;
