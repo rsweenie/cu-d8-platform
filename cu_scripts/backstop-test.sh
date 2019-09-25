7a2a1493ce02409d1a860308d9d3589c78c87e4f
Abe Coleman <abraham@abaracus.com>
2019-09-25 13:56:02 -0500

    reference question for bash script

--- cu_scripts/backstop-test.sh
+++ cu_scripts/backstop-test.sh
@@ -15,5 +15,14 @@ fi
 cmd_ref="backstop reference --configPath=backstop.js --pathfile=paths/$site_name --refhost=http://$site_name.creighton.acsitefactory.com/"
 cmd_test="backstop test --configPath=backstop.js --pathfile=paths/$site_name --testhost=http://local.creighton.com/"
 
-eval $cmd_ref 
-eval $cmd_test
+while true; do
+    read -p "Do you want to reference $site_name? " 
+    yn
+    case $yn in
+        [Yy]* ) 
+        eval $cmd_ref 
+        eval $cmd_test break;;
+        [Nn]* ) eval $cmd_test exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
