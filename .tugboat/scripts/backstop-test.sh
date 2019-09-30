c5922580c8b0e6f769d67efe8087698f25b16993
Abe Coleman <abraham@abaracus.com>
2019-09-30 08:30:11 -0500

    renaming variables and hooking correct tugboat url

--- .tugboat/scripts/backstop-test.sh
+++ .tugboat/scripts/backstop-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run Backstop test
 set -e
-echo 'Please enter in the site you would like to test: '
 
-read site_name
+site_name=$(`dirname "$0"`/get_site_alias.sh)
+
 
 if [[ $site_name = "alliance" ]]
 then
@@ -13,15 +13,7 @@ else
 fi
 
 cmd_ref="backstop reference --configPath=backstop.js --pathfile=paths/$site_name --refhost=http://$site_name.creighton.acsitefactory.com/"
-cmd_test="backstop test --configPath=backstop.js --pathfile=paths/$site_name --testhost=http://local.creighton.com/"
+cmd_test="backstop test --configPath=backstop.js --pathfile=paths/$site_name --testhost=$TUGBOAT_SERVICE_URL"
 
-while true; do
-    read -p "Do you want to reference $site_name? " yn
-    case $yn in
-        [Yy]* ) 
-        eval $cmd_ref 
-        eval $cmd_test break;;
-        [Nn]* ) eval $cmd_test exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+eval $cmd_ref 
+eval $cmd_test
