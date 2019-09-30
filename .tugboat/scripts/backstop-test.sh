d220afd9b6f5b08eaaae3f098ab23bb457417e4a
Abe Coleman <abraham@abaracus.com>
2019-09-30 11:23:54 -0500

    site url for alliance

--- .tugboat/scripts/backstop-test.sh
+++ .tugboat/scripts/backstop-test.sh
@@ -12,7 +12,7 @@ else
     site_url=$site_name'.creighton.acsitefactory.com'
 fi
 
-cmd_ref="./node_modules/backstopjs/cli/index.js reference --configPath=backstop.js --pathfile=paths/$site_name --refhost=http://$site_name.creighton.acsitefactory.com/"
+cmd_ref="./node_modules/backstopjs/cli/index.js reference --configPath=backstop.js --pathfile=paths/$site_name --refhost=http://$site_url"
 cmd_test="./node_modules/backstopjs/cli/index.js test --configPath=backstop.js --pathfile=paths/$site_name --testhost=$TUGBOAT_SERVICE_URL"
 
 eval $cmd_ref 
