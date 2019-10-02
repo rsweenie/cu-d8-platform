44a26ac5dde83eb0e988ad4bc91e424167a6be83
Abe Coleman <abraham@abaracus.com>
2019-10-02 08:23:51 -0500

    simplifying things

--- .tugboat/scripts/backstop-test.sh
+++ .tugboat/scripts/backstop-test.sh
@@ -5,17 +5,10 @@ site_name=$($TUGBOAT_ROOT/.tugboat/scripts/get_site_alias.sh)
 
 cd $TUGBOAT_ROOT/tests/backstopjs
 
-if [[ $site_name = "alliance" ]]
-then
-    site_url=$site_name'.creighton.edu'
-else
-    site_url=$site_name'.creighton.acsitefactory.com'
-fi
-
-cmd_ref="./node_modules/backstopjs/cli/index.js reference --configPath=backstop.js --pathfile=paths/$site_name --refhost=http://$site_name'.creighton.acsitefactory.com'"
-cmd_test="./node_modules/backstopjs/cli/index.js test --configPath=backstop.js --pathfile=paths/$site_name --testhost=$TUGBOAT_SERVICE_URL"
+cmd_ref="./node_modules/backstopjs/cli/index.js reference --configPath=backstop.js --pathfile=paths/demo --refhost=http://demo.creighton.acsitefactory.com"
+#cmd_test="./node_modules/backstopjs/cli/index.js test --configPath=backstop.js --pathfile=paths/demo --testhost=$TUGBOAT_SERVICE_URL"
 
 
 echo $site_name
 eval $cmd_ref 
-eval $cmd_test
+#eval $cmd_test
