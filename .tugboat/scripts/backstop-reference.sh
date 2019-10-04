6f580d977ed40d86cf6c8baf32750d840be3cf38
Abe Coleman <abraham@abaracus.com>
2019-10-04 13:14:19 -0500

    reference testing vs live testing

---
+++ .tugboat/scripts/backstop-reference.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Run Backstop test
+
+site_name=$($TUGBOAT_ROOT/.tugboat/scripts/get_site_alias.sh)
+
+cd $TUGBOAT_ROOT/tests/backstopjs
+
+cmd_ref="./node_modules/backstopjs/cli/index.js reference --configPath=backstop.js --pathfile=paths/$site_name --refhost=http://$site_name.creighton.acsitefactory.com"
+
+eval $cmd_ref 
+
+echo "Copy and paste the below to run the backstop test:
+/bin/bash backstop-test.sh"
+
--- .tugboat/scripts/backstop-test.sh
+++ .tugboat/scripts/backstop-test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Run Backstop test
 
-
 site_name=$($TUGBOAT_ROOT/.tugboat/scripts/get_site_alias.sh)
 
 cd $TUGBOAT_ROOT/tests/backstopjs
 
-cmd_ref="./node_modules/backstopjs/cli/index.js reference --configPath=backstop.js --pathfile=paths/demo --refhost=http://demo.creighton.acsitefactory.com"
-cmd_test="./node_modules/backstopjs/cli/index.js test --configPath=backstop.js --pathfile=paths/demo --testhost=$TUGBOAT_SERVICE_URL"
+cmd_test="./node_modules/backstopjs/cli/index.js test --configPath=backstop.js --pathfile=paths/$site_name --testhost=$TUGBOAT_SERVICE_URL"
 
-
-echo $site_name
-eval $cmd_ref 
 eval $cmd_test
-echo $TUGBOAT_SERVICE_URL'sites/default/files/html_report/'
+
+echo "Copy and paste the below to run the test:
+cd $TUGBOAT_ROOT/tests/backstopjs; ./node_modules/backstopjs/cli/index.js test --configPath=backstop.js --pathfile=paths/$site_name --testhost=$TUGBOAT_SERVICE_URL
+"
+echo $TUGBOAT_ROOT'/sites/default/files/html_report/'
+
