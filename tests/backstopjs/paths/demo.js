ab6765f0319e3ea88e586c58e66c0bc7e0c439f9
Abe Coleman <abraham@abaracus.com>
2019-10-04 14:31:36 -0500

    demo urls

--- tests/backstopjs/paths/demo.js
+++ tests/backstopjs/paths/demo.js
@@ -2,5 +2,17 @@ var pathConfig = {};
 pathConfig.array = [
     '/',
     '/how-build-site',
+    '/how-build-site/content-types/copy-box',
+    '/news',
+    '/how-build-site/content-types/featured-content-group',
+    '/how-build-site/content-types/image-slider',
+    '/how-build-site/content-types/featured-links',
+    '/how-build-site/content-types/profiles',
+    '/how-build-site/content-types/promo-box',
+    '/how-build-site/content-types/quote-box',
+    '/how-build-site/content-types/related-links',
+    '/how-build-site/content-types/spotlight',
+    '/how-build-site/content-types/tabbed-accordion',   
+    '/how-build-site/content-types/content-page'
 ]
 module.exports = pathConfig;
