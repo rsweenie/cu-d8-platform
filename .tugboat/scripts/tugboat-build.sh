0d30203f2cf994efc7aa21efe62eb0698f9da490
Robert Wohleb <rwohleb@r2integrated.com>
2019-10-06 23:37:09 -0700

    Fixed issue with rsync.

--- .tugboat/scripts/tugboat-build.sh
+++ .tugboat/scripts/tugboat-build.sh
@@ -15,7 +15,7 @@ case $CU_SITE_ALIAS in
     # DB sync MUST come before filesync always
     drush -r "${DOCROOT}" sql:drop -y
     drush -r "${DOCROOT}" sql:sync "@${CU_SITE_ALIAS}.01live" @self -y
-    drush -r "${DOCROOT}" rsync "@${CU_SITE_ALIAS}.01live":%files @self:%files -y
+    drush -r "${DOCROOT}" rsync "@${CU_SITE_ALIAS}.01live":%files @self:%files -y -- --exclude '/styles/'
   ;;
   grad-site)
     echo "Importing local DB file for Grad Site"
