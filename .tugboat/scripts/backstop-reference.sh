ba55b544a44c7ba8b38582e808edbc4d4fde3038
Abe Coleman <abraham@abaracus.com>
2019-10-09 15:06:45 -0500

    full path again arggh

--- .tugboat/scripts/backstop-reference.sh
+++ .tugboat/scripts/backstop-reference.sh
@@ -5,7 +5,7 @@ site_name=$($TUGBOAT_ROOT/.tugboat/scripts/get_site_alias.sh)
 
 cd /$TUGBOAT_ROOT/tests/backstopjs
 
-cmd_ref="./node_modules/backstopjs/cli/index.js reference --configPath=backstop.js --pathfile=paths/$site_name --refhost=http://$site_name.creighton.acsitefactory.com"
+cmd_ref="/usr/local/lib/node_modules/backstopjs/cli/index.js reference --configPath=backstop.js --pathfile=paths/$site_name --refhost=http://$site_name.creighton.acsitefactory.com"
 
 eval $cmd_ref 
 
--- .tugboat/scripts/backstop-test.sh
+++ .tugboat/scripts/backstop-test.sh
@@ -5,7 +5,7 @@ site_name=$($TUGBOAT_ROOT/.tugboat/scripts/get_site_alias.sh)
 
 cd $TUGBOAT_ROOT/tests/backstopjs
 
-cmd_test="./node_modules/backstopjs/cli/index.js test --configPath=backstop.js --pathfile=paths/$site_name --testhost=$TUGBOAT_SERVICE_URL"
+cmd_test="/usr/local/lib/node_modules/backstopjs/cli/index.js test --configPath=backstop.js --pathfile=paths/$site_name --testhost=$TUGBOAT_SERVICE_URL"
 
 eval $cmd_test
 
