2dbb44786589530a9779eabf0f2170af02fb7680
Robert Wohleb <rwohleb@r2integrated.com>
2019-10-10 14:29:01 -0700

    CU-489: Improved change info on type def page.

--- docroot/modules/custom/cu_hub_consumer/src/Form/HubResourceTypeDefinitionUpdateForm.php
+++ docroot/modules/custom/cu_hub_consumer/src/Form/HubResourceTypeDefinitionUpdateForm.php
@@ -94,23 +94,8 @@ class HubResourceTypeDefinitionUpdateForm extends FormBase {
 
       $tracked_types = HubResourceTypeDefinition::loadMultiple();
       $tracked_type_ids = [];
-      $changed_tracked_type_fields = [];
       foreach ($tracked_types as $tracked_type) {
         $tracked_type_ids[$tracked_type->get('type_id')] = $tracked_type->get('type_id');
-
-        // We grab the latest inspection info, update the data in the def object, but then we
-        // don't actually save it. We are just using it to detect differences. Saving of changes
-        // will happen on form submit.
-        try {
-          $inspection_info = $this->resourceInspector->inspect($tracked_type->get('type_id'));
-          $tracked_type->set('fields', $inspection_info);
-          if ($tracked_type->hasChanged()) {
-            $changed_tracked_type_fields[$tracked_type->get('type_id')] = $tracked_type->getChanges();
-          }
-        }
-        catch (ClientException $e) {
-          $this->messenger->addError($e->getMessage());
-        }
       }
 
       $form['resource_types_message'] = [
@@ -118,20 +103,57 @@ class HubResourceTypeDefinitionUpdateForm extends FormBase {
         '#title' => $this->t('Resource types to track'),
         '#markup' => $this->t('A resource type must be tracked here if it will be displayed or processed.'),
       ];
-      if (!empty($changed_tracked_type_fields)) {
-        $form['resource_types_changed_fields'] = [
-          '#type' => 'details',
-          '#title' => $this->t('Tracked definition changes'),
-          '#description' => $this->t('The following resource type definitions differ from the inspection of hub.'),
-          '#open' => TRUE,
-        ];
-        foreach ($changed_tracked_type_fields as $type_id => $field_names) {
-          $form['resource_types_changed_fields'][$type_id] = [
-            '#type' => 'item',
-            '#markup' => '<b>' . $type_id . '</b>: ' . implode(', ', $field_names),
-          ];
+
+      $form['resource_types_changed_fields'] = [
+        '#type' => 'details',
+        '#title' => $this->t('Tracked definition changes'),
+        '#description' => $this->t('The following resource type definitions differ from the inspection of hub.'),
+        '#open' => TRUE,
+      ];
+
+      foreach ($tracked_types as $tracked_type) {
+        $type_id = $tracked_type->get('type_id');
+
+        // We grab the latest inspection info, update the data in the def object, but then we
+        // don't actually save it. We are just using it to detect differences. Saving of changes
+        // will happen on form submit.
+        try {
+          $inspection_info = $this->resourceInspector->inspect($type_id);
+          $tracked_type->set('fields', $inspection_info);
+          if (empty($inspection_info)) {
+            $form['resource_types_changed_fields'][$type_id] = [
+              '#type' => 'item',
+              '#markup' => '<b>' . $type_id . '</b>: Empty inspection results. At least one must exist on hub for this to be inspected.',
+            ];
+          }
+          elseif ($changes = $tracked_type->getChanges()) {
+            $changed_fields = [];
+            if (!empty($changes['added'])) {
+              foreach ($changes['added'] as $field_name => $props) {
+                $changed_fields[] = '[+] ' . $field_name;
+              }
+            }
+            if (!empty($changes['removed'])) {
+              foreach ($changes['removed'] as $field_name => $props) {
+                $changed_fields[] = '[-] ' . $field_name;
+              }
+            }
+            if (!empty($changes['modified'])) {
+              foreach ($changes['modified'] as $field_name => $props) {
+                $changed_fields[] = '[m] ' . $field_name;
+              }
+            }
+            $form['resource_types_changed_fields'][$type_id] = [
+              '#type' => 'item',
+              '#markup' => '<b>' . $type_id . '</b>: ' . implode(', ', $changed_fields),
+            ];
+          }
+        }
+        catch (ClientException $e) {
+          $this->messenger->addError($e->getMessage());
         }
       }
+
       $form['resource_types'] = [
         '#type' => 'checkboxes',
         '#options' => $resource_type_options,
@@ -160,40 +182,42 @@ class HubResourceTypeDefinitionUpdateForm extends FormBase {
     $has_changed = FALSE;
 
     foreach ($resource_types as $resource_type) {
-      if ($resource_type_info = $this->resourceInspector->inspect($resource_type)) {
-        // Are we updating?
-        if ($type_defs = $def_storage->loadByProperties(['type_id' => $resource_type])) {
-          foreach ($type_defs as $type_def) {
-            $type_def->set('fields', $resource_type_info);
-            if ($type_def->hasChanged()) {
-              $has_changed = TRUE;
-              if ($type_def->save()) {
-                $this->messenger->addStatus($this->t('Updated %type_id definition.', ['%type_id' => $resource_type]));
-              }
-              else {
-                $this->messenger->addWarning($this->t('Failed to update %type_id definition.', ['%type_id' => $resource_type]));
-              }
+      $resource_type_info = $this->resourceInspector->inspect($resource_type);
+      $resource_type_defs = $def_storage->loadByProperties(['type_id' => $resource_type]);
+      
+      // Are we updating?
+      if ($resource_type_info && $resource_type_defs) {
+        foreach ($resource_type_defs as $type_def) {
+          $type_def->set('fields', $resource_type_info);
+          if ($type_def->hasChanged()) {
+            $has_changed = TRUE;
+            if ($type_def->save()) {
+              $this->messenger->addStatus($this->t('Updated %type_id definition.', ['%type_id' => $resource_type]));
+            }
+            else {
+              $this->messenger->addWarning($this->t('Failed to update %type_id definition.', ['%type_id' => $resource_type]));
             }
           }
         }
-        // Else, we need to create a new one.
-        else {
-          $has_changed = TRUE;
-          $type_def = HubResourceTypeDefinition::create([
-            'id' => $this->resourceInspector->getResourceTypeMachineId($resource_type),
-            'type_id' => $resource_type,
-            'fields' => $resource_type_info,
-          ]);
-          if ($type_def->save()) {
-            $this->messenger->addStatus($this->t('Created %type_id definition.', ['%type_id' => $resource_type]));
-          }
-          else {
-            $this->messenger->addWarning($this->t('Failed to create %type_id definition.', ['%type_id' => $resource_type]));
-          }
-        }
       }
+      // Else, if we already have a def, but the inspection is empty, we change nothing and give a warning.
+      elseif (!$resource_type_info && $resource_type_defs) {
+        $this->messenger->addWarning($this->t('Failed to update %type_id definition. The inspection info was empty.', ['%type_id' => $resource_type]));
+      }
+      // Else, we need to create a new one.
       else {
-        $this->messenger->addWarning($this->t('Inspection failed for %type_id. At least one must exist on hub for this to be inspected.', ['%type_id' => $resource_type]));
+        $has_changed = TRUE;
+        $type_def = HubResourceTypeDefinition::create([
+          'id' => $this->resourceInspector->getResourceTypeMachineId($resource_type),
+          'type_id' => $resource_type,
+          'fields' => $resource_type_info,
+        ]);
+        if ($type_def->save()) {
+          $this->messenger->addStatus($this->t('Created %type_id definition.', ['%type_id' => $resource_type]));
+        }
+        else {
+          $this->messenger->addWarning($this->t('Failed to create %type_id definition.', ['%type_id' => $resource_type]));
+        }
       }
     }
 
